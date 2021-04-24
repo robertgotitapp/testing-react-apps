8839a41c36d32184481f85a7bc0e3152f4e4a8e2
Robert Nguyen <robert@gotitapp.co>
2021-04-24 18:27:28 +0700

    feat: exercises and extra credits for 2, 3, 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,24 +4,52 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import faker from 'faker'
+const { build, fake } = require('@jackfranklin/test-data-bot');
 import Login from '../../components/login'
 
+
+// const buildLoginForm = (priority = {username: null, password: null}) => {
+//   const fakeUsername = faker.internet.userName()
+//   const fakePassword = faker.internet.password()
+
+//   return {
+//     username: priority.username || fakeUsername,
+//     password: priority.password || fakePassword
+//   }
+// }
+
+const userBuilder = build({
+  fields: {
+    username: fake(f => f.name.findName()),
+    password: fake(f => f.internet.password())
+  }
+})
+
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  const handleSubmit = jest.fn()
+  
+  render(<Login onSubmit={handleSubmit} />)
+  // screen.debug()
+
+  // const {username, password} = buildLoginForm({username: 'robert'})
+  const { username, password } = userBuilder()
+
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+
+  const submitButton = screen.getByRole('button', {
+    name: /submit/i
+  })
+
+  userEvent.type(usernameInput, username)
+  userEvent.type(passwordInput, password)
+  userEvent.click(submitButton)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    password,
+    username,
+  })
+
 })
 
 /*
